0e85a367462cd29201d43eb85bbb95737b121659
Jeff H <jahuangi@yahoo.ca>
2015-08-26 19:05:49 -0700

    added missing category_name function

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,13 @@
 class Snippet < ActiveRecord::Base
 
+  belongs_to :category
+
   validates :title, presence: true, uniqueness: true
   validates :work, presence: true, uniqueness: true
+
+  #delegate :name, to: :category # @question.name
+  delegate :name, to: :category, prefix: true # @question.category_name
+  # +  # def category_name
+  # +  #   category.name
+  # +  # end
 end
